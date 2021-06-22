75c059f19c2bbdff11b324a35080f73cb2c53f87
Arhaam Patvi <thearhaam@gmail.com>
2021-06-22 08:36:58 +0530

    test: initial access tests

---
+++ test/ShowtimeMT.test.js
@@ -0,0 +1,56 @@
+const MT = artifacts.require("ShowtimeMT");
+const truffleAssert = require("truffle-assertions");
+
+contract("ShowtimeMT", async (accounts) => {
+    describe("Access Tests", async () => {
+        beforeEach(async () => {
+            // Deploy MultiToken
+            await MT.new();
+            this.mt = await MT.deployed();
+            // Set Admin
+            this.mt.setAdmin(accounts[1], true);
+        });
+        it("non-admin should not be able to mint", async () => {
+            const recepient = accounts[1];
+            const amount = 10;
+            const hash = "some-hash";
+            const data = "some-data";
+            try {
+                var result = await this.mt.issueToken(recepient, amount, hash, JSON.stringify(data), { from: accounts[2] });
+            }
+            catch (err) {
+                if (err.reason == "AccessProtected: caller is not admin")
+                    return;
+                else
+                    return false;
+            }
+            truffleAssert.eventNotEmitted(result, 'Transfer');
+            return false;
+        });
+        it("owner should be able to set admin", async () => {
+            const result = await this.mt.setAdmin(accounts[2], true);
+            truffleAssert.eventEmitted(result, 'AdminAccessSet');
+            const isAdmin = await this.mt.isAdmin(accounts[2]);
+            assert.equal(isAdmin, true);
+        });
+        it("non-owner should not be able to set admin", async () => {
+            try {
+                var result = await this.mt.setAdmin(accounts[1], true, { from: accounts[1] });
+            }
+            catch (err) {
+                if (err.reason == "AccessProtected: caller is not admin")
+                    return;
+                else
+                    return false;
+            }
+            truffleAssert.eventNotEmitted(result, 'AdminAccessSet');
+            return false;
+        });
+        it("owner should be able to revoke admin", async () => {
+            const result = await this.mt.setAdmin(accounts[1], false);
+            truffleAssert.eventEmitted(result, 'AdminAccessSet');
+            const isAdmin = await this.mt.isAdmin(accounts[1]);
+            assert.equal(isAdmin, false);
+        });
+    })
+})
