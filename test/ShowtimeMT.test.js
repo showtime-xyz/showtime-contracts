cd544c44c117dcc71263bc4d75252387bd485361
Arhaam Patvi <thearhaam@gmail.com>
2021-06-24 21:43:55 +0530

    test: minting & transfer tests

--- test/ShowtimeMT.test.js
+++ test/ShowtimeMT.test.js
@@ -22,9 +22,21 @@ contract("ShowtimeMT", async (accounts) => {
         });
         it("owner should be able to set admin", async () => {
             const result = await this.mt.setAdmin(accounts[2], true);
-            truffleAssert.eventEmitted(result, 'AdminAccessSet');
+            truffleAssert.eventEmitted(result, 'UserAccessSet');
             const isAdmin = await this.mt.isAdmin(accounts[2]);
             assert.equal(isAdmin, true);
+        });
+        it("admin should be able to set minter", async () => {
+            const result = await this.mt.setMinter(accounts[3], true, { from: accounts[2] });
+            truffleAssert.eventEmitted(result, 'UserAccessSet');
+            const isMinter = await this.mt.isMinter(accounts[3]);
+            assert.equal(isMinter, true);
+        });
+        it("admin should be able to revoke minter", async () => {
+            const result = await this.mt.setMinter(accounts[3], false, { from: accounts[2] });
+            truffleAssert.eventEmitted(result, 'UserAccessSet');
+            const isMinter = await this.mt.isMinter(accounts[3]);
+            assert.equal(isMinter, false);
         });
         it("non-owner should not be able to set admin", async () => {
             await truffleAssert.reverts(
@@ -34,9 +46,27 @@ contract("ShowtimeMT", async (accounts) => {
         });
         it("owner should be able to revoke admin", async () => {
             const result = await this.mt.setAdmin(accounts[1], false);
-            truffleAssert.eventEmitted(result, 'AdminAccessSet');
+            truffleAssert.eventEmitted(result, 'UserAccessSet');
             const isAdmin = await this.mt.isAdmin(accounts[1]);
             assert.equal(isAdmin, false);
+        });
+    });
+    describe("Minting Tests", async () => {
+        beforeEach(async () => {
+            // Deploy MultiToken
+            await MT.new();
+            this.mt = await MT.deployed();
+        });
+        it("minter should be able to mint", async () => {
+            const recipient = accounts[1];
+            const amount = 10;
+            const hash = "some-hash";
+            const data = "0x00";
+            await this.mt.issueToken(recipient, amount, hash, data);
+            const balance = await this.mt.balanceOf(recipient, 1);
+            assert.equal(balance, amount);
+            const URI = await this.mt.uri(1);
+            assert.equal(URI, "https://gateway.pinata.cloud/ipfs/some-hash");
         });
     });
     describe("URI Tests", async () => {
@@ -64,6 +94,33 @@ contract("ShowtimeMT", async (accounts) => {
         it("should be able to get token URI", async () => {
             const URI = await this.mt.uri(1);
             assert.equal(URI, "https://gateway.test.com/ipfs/some-hash");
+        });
+    });
+    describe("Transfer Tests", async () => {
+        beforeEach(async () => {
+            // Deploy MultiToken
+            await MT.new();
+            this.mt = await MT.deployed();
+            // Issue MT
+            const recipient = accounts[1];
+            const amount = 10;
+            const hash = "some-hash";
+            const data = "0x00";
+            await this.mt.issueToken(recipient, amount, hash, data);
+        });
+        it("nft-owner should be able to transfer amount", async () => {
+            const from = accounts[1];
+            const to = accounts[2];
+            const id = 1;
+            const amount = 1;
+            const data = "0x00";
+            const fromBalance = await this.mt.balanceOf(from, id);
+            const toBalance = await this.mt.balanceOf(to, id);
+            await this.mt.safeTransferFrom(from, to, id, amount, data, { from });
+            const fromBalance_new = await this.mt.balanceOf(from, id);
+            const toBalance_new = await this.mt.balanceOf(to, id);
+            assert.equal(BigInt(fromBalance_new), BigInt(fromBalance - amount));
+            assert.equal(BigInt(toBalance_new), BigInt(toBalance + amount));
         });
     });
 })
