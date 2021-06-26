9e38d43949a0a9347fd83bd85a60835d590458ca
Arhaam Patvi <thearhaam@gmail.com>
2021-06-27 02:24:36 +0530

    feat: public minting + set minter batch

--- contracts/utils/AccessProtected.sol
+++ contracts/utils/AccessProtected.sol
@@ -7,6 +7,7 @@ import "@openzeppelin/contracts/utils/Context.sol";
 abstract contract AccessProtected is Context, Ownable {
     mapping(address => bool) private _admins; // user address => admin? mapping
     mapping(address => bool) private _minters; // user address => minter? mapping
+    bool public publicMinting;
 
     event UserAccessSet(address _user, string _access, bool _enabled);
 
@@ -17,6 +18,7 @@ abstract contract AccessProtected is Context, Ownable {
      * @param enabled - Enable/Disable Admin Access
      */
     function setAdmin(address admin, bool enabled) external onlyOwner {
+        require(admin != address(0), "Invalid Admin Address");
         _admins[admin] = enabled;
         emit UserAccessSet(admin, "ADMIN", enabled);
     }
@@ -27,9 +29,36 @@ abstract contract AccessProtected is Context, Ownable {
      * @param minter - Address of Minter
      * @param enabled - Enable/Disable Admin Access
      */
-    function setMinter(address minter, bool enabled) external onlyAdmin {
+    function setMinter(address minter, bool enabled) public onlyAdmin {
+        require(minter != address(0), "Invalid Minter Address");
         _minters[minter] = enabled;
         emit UserAccessSet(minter, "MINTER", enabled);
+    }
+
+    /**
+     * @notice Set Minter Access
+     *
+     * @param minters - Address of Minters
+     * @param enabled - Enable/Disable Admin Access
+     */
+    function setMinters(address[] calldata minters, bool enabled)
+        external
+        onlyAdmin
+    {
+        for (uint256 i = 0; i < minters.length; i++) {
+            address minter = minters[i];
+            setMinter(minter, enabled);
+        }
+    }
+
+    /**
+     * @notice Enable/Disable public Minting
+     *
+     * @param enabled - Enable/Disable
+     */
+    function setPublicMinting(bool enabled) external onlyAdmin {
+        publicMinting = enabled;
+        emit UserAccessSet(address(0), "MINTER", enabled);
     }
 
     /**
@@ -68,7 +97,8 @@ abstract contract AccessProtected is Context, Ownable {
      */
     modifier onlyMinter() {
         require(
-            _minters[_msgSender()] ||
+            publicMinting ||
+                _minters[_msgSender()] ||
                 _admins[_msgSender()] ||
                 _msgSender() == owner(),
             "AccessProtected: caller is not minter"
