77cf0e7e98330789e9fe45b367518d0a8cd78770
aster2709 <ymehta777@gmail.com>
2021-07-26 16:41:28 +0530

    add royalties support with EIP2981

---
+++ contracts/ERC2981Royalties.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.12;
+import "./IERC2981.sol";
+
+contract ERC2981Royalties is IERC2981 {
+    struct Royalty {
+        address recipient;
+        uint256 value; // as a % unit, from 0 - 10000 (2 extra 0s) for eg 25% is 2500
+    }
+
+    mapping(uint256 => Royalty) internal _royalties; // tokenId => royalty
+
+    function _setTokenRoyalty(
+        uint256 id,
+        address recipient,
+        uint256 value
+    ) internal {
+        require(value <= 10000, "ERC2981Royalties: value too high");
+        _royalties[id] = Royalty(recipient, value);
+    }
+
+    function royaltyInfo(uint256 _tokenId, uint256 _salePrice)
+        external
+        view
+        override
+        returns (address receiver, uint256 royaltyAmount)
+    {
+        Royalty memory royalty = _royalties[_tokenId];
+        return (royalty.recipient, (_salePrice * royalty.value) / 10000);
+    }
+}
