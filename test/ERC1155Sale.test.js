6e7d8834e95c221a94b51edd37757be6f6397526
aster2709 <ymehta777@gmail.com>
2021-08-18 19:50:36 +0530

    test: use showtime mt contracts instead of a test erc1155

--- test/ERC1155Sale.test.js
+++ test/ERC1155Sale.test.js
@@ -1,40 +1,39 @@
-const NFT = artifacts.require("ERC1155NFT")
+const MT = artifacts.require("ShowtimeMT")
 const Token = artifacts.require("Token")
 const ERC1155Sale = artifacts.require("ERC1155Sale")
 const truffleAsserts = require("truffle-assertions")
 const expect = require("chai").expect
 
 contract("ERC1155 Sale Contract Tests", (accounts) => {
-    let nft, token, sale, admin, alice, bob
+    let mt, token, sale, admin, alice, bob
     beforeEach(async () => {
         admin = accounts[0]
         alice = accounts[1]
         bob = accounts[2]
-        // mint 10 of id 1
-        nft = await NFT.new()
-        await nft.mint(1, 10, "0x0", { from: alice })
+        // mint 10
+        mt = await MT.new()
+        await mt.issueToken(alice, 10, "some-hash", "0x0", "0x".padEnd(42, "0"), 0)
         // mint erc20s
         token = await Token.new()
         await token.mint(500, { from: bob })
         // approve the sale contract
-        sale = await ERC1155Sale.new(nft.address, token.address)
-        await nft.setApprovalForAll(sale.address, true, { from: alice })
+        sale = await ERC1155Sale.new(mt.address, token.address)
+        await mt.setApprovalForAll(sale.address, true, { from: alice })
         await token.approve(sale.address, 500, { from: bob })
     })
     it("deploys correctly", async () => {
         // should revert on non contract address
         await truffleAsserts.reverts(ERC1155Sale.new(alice, bob), "must be contract address")
-        expect(await sale.nft()).eq(nft.address)
+        expect(await sale.nft()).eq(mt.address)
         expect(await sale.quoteToken()).eq(token.address)
     })
-
     it("creates a new sale", async () => {
         // alice creates a sale
         const { args: New } = (await sale.createSale(1, 5, 500, { from: alice })).logs[0]
         expect(New.saleId.toString()).eq("0")
         expect(New.seller).eq(alice)
         expect(New.tokenId.toString()).eq("1")
-        expect((await nft.balanceOf(alice, 1)).toString()).eq("5") // 10 - 5
+        expect((await mt.balanceOf(alice, 1)).toString()).eq("5") // 10 - 5
         const res = await sale.sales(New.saleId.toString())
         expect(res.tokenId.toString()).eq("1")
         expect(res.amount.toString()).eq("5")
@@ -60,7 +59,7 @@ contract("ERC1155 Sale Contract Tests", (accounts) => {
         expect(Buy.buyer).eq(bob)
         const res = await sale.sales(Buy.saleId.toString())
         expect(res.isActive).to.be.false
-        expect((await nft.balanceOf(bob, 1)).toString()).eq("5")
+        expect((await mt.balanceOf(bob, 1)).toString()).eq("5")
     })
     it("only owner can pause and unpause", async () => {
         await truffleAsserts.reverts(sale.pause({ from: alice }), "Ownable: caller is not the owner")
