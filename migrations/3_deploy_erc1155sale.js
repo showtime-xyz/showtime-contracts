f6f3521eac0f41b50b4ba6f688151f8e733908d1
aster2709 <ymehta777@gmail.com>
2021-10-08 15:25:14 +0530

    chore: set up trusted forwarder in the migration file

--- migrations/3_deploy_erc1155sale.js
+++ migrations/3_deploy_erc1155sale.js
@@ -1,9 +1,19 @@
 const MT = artifacts.require("ShowtimeMT");
 const ERC1155Sale = artifacts.require("ERC1155Sale");
 
-module.exports = async function (deployer) {
+const MUMBAI_FORWARDER = "0x9399BB24DBB5C4b782C70c2969F58716Ebbd6a3b";
+const POLYGON_FORWARDER = "0x86C80a8aa58e0A4fa09A69624c31Ab2a6CAD56b8";
+
+module.exports = async function (deployer, network) {
     const mt = await MT.new();
-    console.log("nft address:", mt.address);
     const sale = await ERC1155Sale.new(mt.address, []);
-    console.log("sale address:", sale.address);
+    if (network === "mumbai_testnet") {
+        await sale.setTrustedForwarder(MUMBAI_FORWARDER);
+    } else if (network === "matic_mainnet") {
+        await sale.setTrustedForwarder(POLYGON_FORWARDER);
+    }
+    console.log({
+        mt: mt.address,
+        sale: sale.address,
+    });
 };
