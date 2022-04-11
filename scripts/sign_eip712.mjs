fa923c01f45cfe882c5be5a142af68355c330c31
karmacoma <karmacoma.eth@gmail.com>
2022-04-11 13:35:34 -0700

    Add scripts/sign_eip712.mjs and forward request test

---
+++ scripts/sign_eip712.mjs
@@ -0,0 +1,76 @@
+import {ethers} from "ethers";
+
+const forwarderABI = ["function getNonce(address from) view returns(uint256)"];
+
+const USER_ADDRESS = "0xDE5E327cE4E7c1b64A757AaB8f2E699585977a34";
+const USER_PRIVATE_KEY = "0xbfea5ee5076b0bff4a26f7e3b5a8b8093c664a330cb7ab024f041b9ae077fa2e";
+
+const ShowtimeForwarderContract = new ethers.Contract(
+    "0x50c001c88b59dc3b833E0F062EfC2271CE88Cb89",
+    forwarderABI);
+
+const NETWORKS = {
+    mumbai: {
+        chainId: 80001,
+        forwarder: ShowtimeForwarderContract.connect(new ethers.providers.JsonRpcProvider("https://rpc-mumbai.matic.today")),
+    },
+
+    polygon: {
+        chainId: 137,
+        forwarder: ShowtimeForwarderContract.connect(new ethers.providers.JsonRpcProvider("https://polygon-rpc.com")),
+    },
+}
+
+const network = NETWORKS.mumbai;
+
+const domain = {
+    name: 'showtime.io',
+    version: '1',
+    chainId: network.chainId,
+    verifyingContract: network.forwarder.address,
+};
+
+// The named list of all type definitions
+const types = {
+    ForwardRequest: [
+        { name: 'from', type: 'address' },
+        { name: 'to', type: 'address' },
+        { name: 'value', type: 'uint256' },
+        { name: 'gas', type: 'uint256' },
+        { name: 'nonce', type: 'uint256' },
+        { name: 'data', type: 'bytes' },
+        { name: 'validUntilTime', type: 'uint256' },
+    ]
+};
+
+// these values depend on the contract we are trying to call
+let targetAddress = '0x185a4dc360CE69bDCceE33b3784B0282f7961aea';
+let targetInterface = new ethers.utils.Interface([
+        "function emitMessage(string memory message)"
+    ]);
+let callData = targetInterface.encodeFunctionData("emitMessage", ["hello"]);
+
+// The data to sign
+const value = {
+    from: USER_ADDRESS,
+    to: targetAddress,
+    value: 0,
+
+    // we can hardcode a sufficiently high value for now
+    gas: 1000000,
+
+    // this is the one remote call we need to make before signing
+    nonce: await network.forwarder.getNonce(USER_ADDRESS),
+    data: callData,
+    validUntilTime: 7697467249,
+};
+
+console.log('Signing data for domain:');
+console.log({domain});
+console.log('With the following ForwardRequest:');
+console.log({value});
+
+// the provider does not actually matter, this is a local operation
+const signer = new ethers.Wallet(USER_PRIVATE_KEY, ethers.getDefaultProvider('mainnet'));
+const signature = await signer._signTypedData(domain, types, value);
+console.log({signature});
--- src/test/periphery/ShowtimeForwarderTest.sol
+++ src/test/periphery/ShowtimeForwarderTest.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.7;
 
 import {ShowtimeForwarder} from "../../periphery/ShowtimeForwarder.sol";
-import {TestForwarder} from "../../lib/gsn/forwarder/test/TestForwarder.sol";
-import {TestForwarderTarget} from "../../lib/gsn/forwarder/test/TestForwarderTarget.sol";
+import {TestForwarder} from "gsn/forwarder/test/TestForwarder.sol";
+import {TestForwarderTarget} from "gsn/forwarder/test/TestForwarderTarget.sol";
 
 import "../Hevm.sol";
 import "../../../lib/ds-test/src/test.sol";
@@ -13,14 +13,58 @@ contract ShowtimeForwarderTest is DSTest {
     TestForwarderTarget target = new TestForwarderTarget(address(forwarder));
 
     function setUp() public {
-        forwarder.registerDomainSeparator("showtime.io", 1);
     }
 
-    function testSanity() public {
-        assertEq(true, true);
+    function testForward() public {
+        // // verifying contract: 0xce71065d4017f316ec606fe4422e11eb2c47c246
+        // emit log("verifying contract:");
+        // emit log_address(address(forwarder));
+
+        // // target address: 0x185a4dc360ce69bdccee33b3784b0282f7961aea
+        // emit log("target address:");
+        // emit log_address(address(target));
+
+        // emit log("chain id:");
+        // emit log_uint(getChainId());
+
+        // bytes memory data = abi.encodeWithSignature("emitMessage(string)", "hello");
+        // emit log("encoded function call data:");
+        // emit log_bytes(data);
+
+        // the domain separator depends on the address of the forwarder and the chain id, update accordingly
+        forwarder.registerDomainSeparator("showtime.io", "1");
+        bytes32 domainSeparator = 0xd4902b142b24e322de5733208247036df197359c2cb2c333baf08873b47f56f1;
+
+        // keccak256("ForwardRequest(address from,address to,uint256 value,uint256 gas,uint256 nonce,bytes data,uint256 validUntilTime)")
+        bytes32 requestTypeHash = 0xb91ae508e6f0e8e33913dec60d2fdcb39fe037ce56198c70a7927d7cd813fd96;
+
+        ShowtimeForwarder.ForwardRequest memory req;
+
+        // Private Key: bfea5ee5076b0bff4a26f7e3b5a8b8093c664a330cb7ab024f041b9ae077fa2e
+        req.from = 0xDE5E327cE4E7c1b64A757AaB8f2E699585977a34;
+        req.to = address(target);
+        req.value = 0;
+        req.gas = 10000;
+        req.nonce = forwarder.getNonce(req.from); // initially 0
+        req.data = data;
+        req.validUntilTime = 7697467249; // some arbitrary time in the year 2213
+
+        bytes memory suffixData = "";
+
+        // generate with `node scripts/sign_eip712.mjs`
+        bytes memory sig = hex"2933c598de4e399921331224f2964da4fbfc6708f0ca5b89c969b463416cd49035c9263317b71fdd993d893f15207ba1926c59b548fcfe9b268e69af73b7bd561c";
+
+        (bool success, bytes memory ret) = forwarder.execute(req, domainSeparator, requestTypeHash, suffixData, sig);
+        assertTrue(success);
+
+        emit log("ret:");
+        emit log_bytes(ret);
     }
 
-    function testInsanity() public {
-        assertEq(true, false);
+    function testDirectCall() public {}
+
+    function getChainId() internal view returns (uint256 chainId) {
+        /* solhint-disable-next-line no-inline-assembly */
+        assembly { chainId := chainid() }
     }
 }
