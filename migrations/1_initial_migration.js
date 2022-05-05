482343402dac2d75c19707e91ffa5f9f67d48107
karmacoma <karmacoma.eth@gmail.com>
2022-05-04 17:17:09 -0700

    Remove truffle bits
    - config (now foundry + hardhat)
    - tests (now foundry)
    - migrations (now hardhat-deploy)

--- migrations/1_initial_migration.js
+++
@@ -1,6 +0,0 @@
-const Migrations = artifacts.require("Migrations");
-
-module.exports = function (deployer, network) {
-  if (network !== "development" || network != "test") return;
-  deployer.deploy(Migrations);
-};
