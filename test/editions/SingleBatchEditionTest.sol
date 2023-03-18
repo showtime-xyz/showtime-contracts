54f69064a8e490f75a90367f3de9374efea76443
karmacoma <karma@coma.lol>
2023-03-17 18:32:27 -0700

    Add EditionFactoryTest
    Biggest change is moving shared code between
    
         EditionFactoryTest
         SingleBatchEditionTest
    
    into EditionFactoryFixture

--- test/editions/SingleBatchEditionTest.sol
+++ test/editions/SingleBatchEditionTest.sol
@@ -7,102 +7,21 @@ import {Test} from "lib/forge-std/src/Test.sol";
 import "nft-editions/interfaces/Errors.sol";
 import {SSTORE2} from "solmate/utils/SSTORE2.sol";
 
-import {EditionFactory, EditionData} from "src/editions/EditionFactory.sol";
-import "test/fixtures/ShowtimeVerifierFixture.sol";
+// import {Attestation, SignedAttestation} from "src/interfaces/IShowtimeVerifier.sol";
+
+import "test/fixtures/EditionFactoryFixture.sol";
 import "src/editions/interfaces/Errors.sol";
 
-contract SingleBatchEditionTest is Test, ShowtimeVerifierFixture {
+contract SingleBatchEditionTest is Test, EditionFactoryFixture {
     event CreatedBatchEdition(
         uint256 indexed editionId, address indexed creator, address editionContractAddress, string tags
     );
 
-    uint256 constant ROYALTY_BPS = 1000;
-    EditionData private DEFAULT_EDITION_DATA = EditionData(
-        address(new SingleBatchEdition()),
-        "name",
-        "description",
-        "animationUrl",
-        "imageUrl",
-        ROYALTY_BPS,
-        "externalUrl",
-        "creatorName",
-        "tag1,tag2"
-    );
-
-    EditionFactory editionFactory;
-    ShowtimeVerifier verifier;
-
-    address creator = makeAddr("creator");
-    address relayer = makeAddr("relayer");
-    address verifierOwner = makeAddr("verifierOwner");
     address claimer = makeAddr("claimer");
     address badActor = makeAddr("badActor");
-    address signerAddr;
-    uint256 signerKey;
 
     function setUp() public {
-        // configure verifier
-        verifier = new ShowtimeVerifier(verifierOwner);
-        (signerAddr, signerKey) = makeAddrAndKey("signer");
-        vm.prank(verifierOwner);
-        verifier.registerSigner(signerAddr, 7);
-
-        // configure editionFactory
-        editionFactory = new EditionFactory(address(verifier));
-    }
-
-    function getVerifier() public view override returns (ShowtimeVerifier) {
-        return verifier;
-    }
-
-    function createEdition(SignedAttestation memory signedAttestation, bytes memory recipients, bytes memory expectedError)
-        public
-        returns (SingleBatchEdition newEdition)
-    {
-        // anyone can broadcast the transaction as long as it has the right signed attestation
-        vm.prank(relayer);
-
-        if (expectedError.length > 0) {
-            vm.expectRevert(expectedError);
-        }
-
-        newEdition = SingleBatchEdition(
-            address(
-                editionFactory.createEdition(
-                    DEFAULT_EDITION_DATA,
-                    recipients,
-                    signedAttestation
-                )
-            )
-        );
-    }
-
-    function createEdition(Attestation memory attestation, bytes memory recipients, bytes memory expectedError)
-        public
-        returns (SingleBatchEdition)
-    {
-        return createEdition(signed(signerKey, attestation), recipients, expectedError);
-    }
-
-    function createEdition(Attestation memory attestation, bytes memory recipients) public returns (SingleBatchEdition) {
-        return createEdition(attestation, recipients, "");
-    }
-
-    function getCreatorAttestation() public view returns (Attestation memory creatorAttestation) {
-        return getCreatorAttestation(creator);
-    }
-
-    function getCreatorAttestation(address creatorAddr) public view returns (Attestation memory creatorAttestation) {
-        // generate a valid attestation for the default edition data
-        uint256 editionId = editionFactory.getEditionId(DEFAULT_EDITION_DATA, creatorAddr);
-        address editionAddr = address(editionFactory.getEditionAtId(DEFAULT_EDITION_DATA.editionImpl, editionId));
-
-        creatorAttestation = Attestation({
-            context: editionAddr,
-            beneficiary: creatorAddr,
-            validUntil: block.timestamp + 2 minutes,
-            nonce: verifier.nonces(creator)
-        });
+        __EditionFactoryFixture_setUp();
     }
 
     /*//////////////////////////////////////////////////////////////
