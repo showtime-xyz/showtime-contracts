34975cff9f21a4b1fe82452dc34566956b4876dd
karmacoma <karma@coma.lol>
2023-03-31 10:54:00 -0700

    Start building out support for multi-batch
    Critically, this fails and needs to be fixed:
    test_createEdition_canNotReuseAttestationForDifferentRecipients

--- test/editions/EditionFactoryTest.sol
+++ test/editions/EditionFactoryTest.sol
@@ -13,8 +13,12 @@ import "src/editions/interfaces/Errors.sol";
 
 
 contract EditionFactoryTest is Test, ShowtimeVerifierFixture, EditionFactoryFixture {
+    address batchImpl;
+
     function setUp() public {
         __EditionFactoryFixture_setUp();
+
+        batchImpl = address(new MockBatchEdition());
     }
 
     function test_createEdition_nullEditionImplReverts() public {
@@ -75,13 +79,92 @@ contract EditionFactoryTest is Test, ShowtimeVerifierFixture, EditionFactoryFixt
     function test_createEdition_canCreateAlternateContract() public {
         address altEditionImpl = address(new MockBatchEdition());
 
-        SingleBatchEdition edition = createEdition(
+        SingleBatchEdition edition = SingleBatchEdition(createEdition(
             altEditionImpl,
             signed(signerKey, getCreatorAttestation(altEditionImpl, DEFAULT_EDITION_DATA, creator)),
             "",
             ""
-        );
+        ));
 
         assertEq(edition.contractURI(), "mock");
+    }
+
+    function test_createEdition_multiBatchSupportHappyPath() public {
+        address edition = editionFactory.createEdition(
+            batchImpl,
+            address(editionFactory),
+            DEFAULT_EDITION_DATA,
+            signed(signerKey, getCreatorAttestation(batchImpl, DEFAULT_EDITION_DATA, creator))
+        );
+
+        Attestation memory attestation = Attestation({
+            context: address(editionFactory),
+            beneficiary: edition,
+            nonce: 0,
+            validUntil: block.timestamp + 120
+        });
+
+        bytes memory recipients_1 = abi.encodePacked(address(this));
+        bytes memory recipients_2 = abi.encodePacked(address(0x1), address(0x2));
+
+        assertEq(editionFactory.mintBatch(recipients_1, signed(signerKey, attestation)), 1);
+        assertEq(editionFactory.mintBatch(recipients_2, signed(signerKey, attestation)), 2);
+    }
+
+    function test_createEdition_multiBatchSupportWrongContextReverts() public {
+        Attestation memory attestation = getCreatorAttestation(batchImpl, DEFAULT_EDITION_DATA, creator);
+        address predicted = attestation.context;
+
+        // tamper with the context before signing
+        attestation.context = address(0xC0FFEE);
+        SignedAttestation memory signedAttestation = signed(signerKey, attestation);
+
+        vm.expectRevert(abi.encodeWithSignature(
+            "AddressMismatch(address,address)",
+            predicted,
+            address(0xC0FFEE)
+        ));
+        editionFactory.createEdition(batchImpl, address(editionFactory), DEFAULT_EDITION_DATA, signedAttestation);
+    }
+
+    function test_createEdition_multiBatchSupportWrongBeneficiaryReverts() public {
+        Attestation memory attestation = Attestation({
+            context: address(editionFactory),
+            beneficiary: batchImpl,
+            nonce: 0,
+            validUntil: block.timestamp + 120
+        });
+
+        SignedAttestation memory signedAttestation = signed(signerKey, attestation);
+
+        vm.expectRevert("UNAUTHORIZED_MINTER");
+        editionFactory.mintBatch(abi.encodePacked(address(this)), signedAttestation);
+    }
+
+    function test_createEdition_canNotReuseAttestationForDifferentRecipients() public {
+        address edition = editionFactory.createEdition(
+            batchImpl,
+            address(editionFactory),
+            DEFAULT_EDITION_DATA,
+            signed(signerKey, getCreatorAttestation(batchImpl, DEFAULT_EDITION_DATA, creator))
+        );
+
+        Attestation memory attestation = Attestation({
+            context: address(editionFactory),
+            beneficiary: edition,
+            nonce: 0,
+            validUntil: block.timestamp + 120
+        });
+
+        SignedAttestation memory signedAttestation = signed(signerKey, attestation);
+
+        bytes memory recipients_1 = abi.encodePacked(address(this));
+        bytes memory recipients_2 = abi.encodePacked(address(0x1), address(0x2));
+
+        editionFactory.mintBatch(recipients_1, signedAttestation);
+
+        // reusing the same attestation for 2 different mints should NOT work!
+        vm.expectRevert();
+        editionFactory.mintBatch(recipients_2, signedAttestation);
     }
 }
